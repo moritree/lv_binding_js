0ccdd49d6c2f483caaa61506ee247901d133d166
zuni <leone.zuni@gmail.com>
2025-02-18 12:22:27 +1300

    Remove unnecessary null

--- demo/mock_os/index.tsx
+++ demo/mock_os/index.tsx
@@ -4,14 +4,14 @@ import { Render } from "lvgljs-ui";
 import React, { useState } from "react";
 
 console.log("INDEX TSX");
-const apps = new Map<string, () => JSX.Element | null>([
+const apps = new Map<string, () => JSX.Element>([
   ["messages", () => <MessageApp />],
 ]);
 
 function Root() {
   const [active, setActive] = useState<string | null>(null);
 
-  if (active && apps.get(active)) return apps.get(active)?.()!;
+  if (active) return apps.get(active)?.()!;
   return <Home apps={apps} activate={(app) => setActive(app)} />;
 }
 
