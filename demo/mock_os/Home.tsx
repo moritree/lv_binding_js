89a8faf67a162726b897e98be2a558bb47100dcf
zuni <leone.zuni@gmail.com>
2025-02-18 12:17:27 +1300

    Move home screen into separate file
    Sometimes loads

---
+++ demo/mock_os/Home.tsx
@@ -0,0 +1,72 @@
+import Colors from "./colors";
+import { Button, Text, View } from "lvgljs-ui";
+import React from "react";
+
+console.log("HOME TSX");
+
+interface HomeProps {
+  apps: Map<string, () => JSX.Element | null>;
+  activate: (app: string) => void;
+}
+
+export default function Home(props: HomeProps) {
+  return (
+    <View style={{ ...style.window, ...style.home }}>
+      {Array.from(props.apps.keys()).map((app, index) => {
+        const gridPos = {
+          "grid-row-pos": Math.floor(index / 2),
+          "grid-column-pos": index % 2,
+        };
+        return (
+          <Button
+            key={app}
+            style={{ ...style.appButton, ...gridPos }}
+            onPressedStyle={style.pressed}
+            onClick={() => props.activate(app)}
+          >
+            <Text style={{ "text-color": Colors.light, "font-size": 24 }}>
+              {app}
+            </Text>
+          </Button>
+        );
+      })}
+    </View>
+  );
+}
+
+const style = {
+  window: {
+    width: "320px",
+    height: "240px",
+    "background-color": Colors.light,
+    padding: 0,
+    "border-radius": 0,
+    "border-width": 0,
+    overflow: "hidden",
+  },
+  home: {
+    display: "grid",
+    "grid-template-columns": "1fr 1fr",
+    "grid-template-rows": "1fr 1fr",
+    padding: "4px",
+    "row-spacing": "4px",
+    "column-spacing": "4px",
+  },
+  appButton: {
+    padding: 0,
+    "border-radius": 0,
+    "border-width": 0,
+    "background-color": Colors.dark,
+    "grid-child": true,
+    "justify-self": "stretch",
+    "align-self": "stretch",
+    display: "flex",
+    "justify-content": "center",
+    "align-content": "center",
+  },
+  pressed: {
+    "border-width": 2,
+    "border-color": Colors.dark,
+    "background-color": Colors.light,
+  },
+};
--- demo/mock_os/index.tsx
+++ demo/mock_os/index.tsx
@@ -1,83 +1,25 @@
+import Home from "./Home";
 import MessageApp from "./MessageApp";
-import Colors from "./colors";
-import { Button, Render, Text, View } from "lvgljs-ui";
+import { Render } from "lvgljs-ui";
 import React, { useState } from "react";
 
-const dark = Colors.dark;
-const light = Colors.light;
+console.log("INDEX TSX");
+const apps = new Map<string, () => JSX.Element | null>([
+  ["messages", () => <MessageApp />],
+]);
 
-function Root() {
+function App() {
   const [active, setActive] = useState<string | null>(null);
-
-  const apps = new Map<string, () => JSX.Element | null>([
-    ["messages", () => <MessageApp />],
-  ]);
 
   return (
     <>
       {active ? (
         apps.get(active)?.()
       ) : (
-        <View style={{ ...style.window, ...style.home }}>
-          {Array.from(apps.keys()).map((app, index) => {
-            const gridPos = {
-              "grid-row-pos": Math.floor(index / 2),
-              "grid-column-pos": index % 2,
-            };
-            return (
-              <Button
-                key={app}
-                style={{ ...style.appButton, ...gridPos }}
-                onPressedStyle={style.pressed}
-                onClick={() => setActive(app)}
-              >
-                <Text style={{ "text-color": light, "font-size": 24 }}>
-                  {app}
-                </Text>
-              </Button>
-            );
-          })}
-        </View>
+        <Home apps={apps} activate={(app) => setActive(app)} />
       )}
     </>
   );
 }
 
-const style = {
-  window: {
-    width: "320px",
-    height: "240px",
-    "background-color": light,
-    padding: 0,
-    "border-radius": 0,
-    "border-width": 0,
-    overflow: "hidden",
-  },
-  home: {
-    display: "grid",
-    "grid-template-columns": "1fr 1fr",
-    "grid-template-rows": "1fr 1fr",
-    padding: "4px",
-    "row-spacing": "4px",
-    "column-spacing": "4px",
-  },
-  appButton: {
-    padding: 0,
-    "border-radius": 0,
-    "border-width": 0,
-    "background-color": dark,
-    "grid-child": true,
-    "justify-self": "stretch",
-    "align-self": "stretch",
-    display: "flex",
-    "justify-content": "center",
-    "align-content": "center",
-  },
-  pressed: {
-    "border-width": 2,
-    "border-color": dark,
-    "background-color": light,
-  },
-};
-
-Render.render(<Root />);
+Render.render(<App />);
