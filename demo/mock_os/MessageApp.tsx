8e59928c8e2fa69187f5acf2cd8d9d8d1f75a77e
Baxter <baxter.berdinner@gmail.com>
2025-02-19 22:26:44 +1300

    Style update message app, update stylesheet

--- demo/mock_os/MessageApp.tsx
+++ demo/mock_os/MessageApp.tsx
@@ -1,6 +1,7 @@
 import Colors from "./colors";
 import { Text, View } from "lvgljs-ui";
 import React from "react";
+import Style from "./style";
 
 interface Message {
   from: string;
@@ -24,75 +25,46 @@ const messages: Message[] = [
 
 export default function MessageApp() {
   return (
-    <View style={style.root}>
+    /* <View style={Style.root}>
       {messages.map((msg, index) => {
-        let viewStyle = style.message;
-        if (msg.from == "You") viewStyle = { ...viewStyle, ...style.you };
+        let viewStyle = Style.message;
+        if (msg.from == "You") viewStyle = { ...viewStyle, ...Style.you };
 
         return (
           <View key={index} style={viewStyle}>
             {msg.from !== "You" ? (
-              <View style={style.labelView}>
-                <Text style={style.labelText}> {msg.from} </Text>
+              <View style={Style.boxBlack}>
+                <Text style={Style.textSmallWhite}>{msg.from}</Text>
               </View>
             ) : (
               <></>
             )}
-            <Text style={style.messageText}> {msg.content} </Text>
+            <Text style={Style.textSmallWhite}>{msg.content}</Text>
           </View>
         );
       })}
+    </View> */
+
+    <View style={Style.containerMain}>
+      {messages.map((msg, index) => (
+        <View key={index} style={Style.containerBlank}>
+          {msg.from === "You" ? (
+            <>
+              <Text style={{...Style.textSmallBlack, 'text-align': 'right'}}>{msg.from}</Text>
+              <View style={{...Style.boxMessage, ...Style.boxMessageRight}}>
+                <Text style={{...Style.textBodyWhite, 'text-align': 'right'}}>{msg.content}</Text> 
+              </View>
+            </>
+          ) : (
+            <>
+              <Text style={Style.textSmallBlack}>{msg.from}</Text>
+              <View style={Style.boxMessage}>
+                <Text style={Style.textBodyWhite}>{msg.content}</Text>
+              </View>
+            </>
+          )}
+        </View>
+      ))}
     </View>
   );
-}
-
-const style = {
-  root: {
-    width: "100%",
-    height: "100%",
-    padding: "2px",
-    margin: 0,
-    overflow: "auto",
-    display: "flex",
-    "flex-direction": "column",
-    "flex-grow": 1,
-    "row-spacing": "2px",
-    "scroll-dir": "bottom",
-    "border-radius": 0,
-    "border-width": 0,
-    "background-color": Colors.light,
-  },
-  labelView: {
-    "border-width": "0px",
-    "border-radius": "0px",
-    padding: 0,
-    margin: 0,
-    "background-color": Colors.dark,
-    width: "auto",
-    height: "auto",
-  },
-  labelText: {
-    "text-color": Colors.light,
-  },
-  message: {
-    "border-width": "0px",
-    "border-radius": "0px",
-    display: "flex",
-    "flex-direction": "row",
-    width: "100%",
-    overflow: "hidden",
-    margin: "0",
-    padding: "0",
-    "background-color": Colors.light,
-    height: "auto",
-  },
-  messageText: {
-    "text-color": Colors.dark,
-  },
-  you: {
-    "border-width": "1px",
-    width: "100%",
-    "border-color": Colors.dark,
-    padding: "2px",
-  },
-};
+} // todo: figure out why boxes arent aligning properly.
