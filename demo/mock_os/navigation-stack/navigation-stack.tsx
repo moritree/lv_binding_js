fe79fcf7430242b0ca9aba25798b45623973612f
zuni <leone.zuni@gmail.com>
2025-02-27 10:46:58 +1300

    Readability

--- demo/mock_os/navigation-stack/navigation-stack.tsx
+++ demo/mock_os/navigation-stack/navigation-stack.tsx
@@ -4,12 +4,11 @@ import React, { useState } from "react";
 export default function NavigationStack(props: {
   root: (push: (app: JSX.Element, title?: string) => void) => JSX.Element;
 }) {
+  const push = (app: JSX.Element, title?: string) =>
+    setStack((prevStack) => [...prevStack, { title: title, view: app }]);
+
   const [stack, setStack] = useState<{ title?: string; view: JSX.Element }[]>([
-    {
-      view: props.root((app: JSX.Element, title?: string) =>
-        setStack((prevStack) => [...prevStack, { title: title, view: app }]),
-      ),
-    },
+    { view: props.root(push) }, // build root view as base of stack
   ]);
 
   if (stack.length == 1) return stack.at(0)!.view;
