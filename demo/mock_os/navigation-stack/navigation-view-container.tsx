0d7a7a5d1b0226a8981fa2e675bb76608827ddfd
Baxter <baxter.berdinner@gmail.com>
2025-02-27 16:50:49 +1300

    Revamped top bar

--- demo/mock_os/navigation-stack/navigation-view-container.tsx
+++ demo/mock_os/navigation-stack/navigation-view-container.tsx
@@ -11,12 +11,37 @@ export default function NavigationViewContainer(props: {
   return (
     <View style={style.root}>
       <View style={style.bar}>
-        <Button style={style.button} onClick={() => props.back()}>
-          <Text>
-            {props.topLevel ? BUILT_IN_SYMBOL.close : BUILT_IN_SYMBOL.left}
-          </Text>
-        </Button>
-        {props.title && <Text style={style.title}>{props.title}</Text>}
+
+        <View style={style.barSection}>
+          <Button style={style.button} onClick={() => props.back()}>
+            <Text>
+              {props.topLevel ? BUILT_IN_SYMBOL.close : BUILT_IN_SYMBOL.left}
+            </Text>
+          </Button>
+        </View>
+
+        <View style={style.barSection}>
+          <Text style={style.title}>{props.title || "ZuniBax OS"}</Text>
+        </View>
+
+        <View style={{...style.barSection, 'justify-content': 'flex-end'}}>
+          <Button style={style.button}>
+            <Text>
+              {BUILT_IN_SYMBOL.bluetooth}
+            </Text>
+          </Button>
+          <Button style={style.button}>
+            <Text>
+              {BUILT_IN_SYMBOL.wifi}
+            </Text>
+          </Button>
+          <Button style={style.button}>
+            <Text>
+              {BUILT_IN_SYMBOL.battery_2}
+            </Text>
+          </Button>
+        </View>
+
       </View>
       {props.view}
     </View>
@@ -38,24 +63,42 @@ const style = {
   },
   bar: {
     width: "100%",
-    height: "40px",
+    height: "24px",
     "background-color": Colors.dark,
     padding: "2px",
     margin: 0,
     "border-radius": 0,
     "border-width": 0,
+    display: "flex",
+    "flex-direction": "row",
+    "justify-content": "space-between",
+  },
+  barSection: {
+    'padding-left': 0,
+    'padding-right': '2px',
+    'padding-top': 0,
+    'padding-bottom': 0,
+    'width': '33%',
+    'height': 'auto',
+    'border-width': 0,
+    'border-radius': 0,
+    'background-color': Colors.dark,
+    'display': 'flex',
+    'flex-direction': 'row',
+    'justify-content': 'flex-start',
+    'column-spacing': "2px",
   },
   title: {
-    "font-size": "20px",
+    'padding-top': '2px',
+    "font-size": "14px",
     "text-color": Colors.light,
-    "padding-top": 4,
-    "text-align": "center",
     width: "100%",
+    "text-align": "center",
   },
   button: {
     "background-color": Colors.dark,
-    height: "34px",
-    width: "34px",
+    height: "20px",
+    width: "20px", 
     "shadow-opacity": 0.0,
     "flex-grow": 0,
     display: "flex",
